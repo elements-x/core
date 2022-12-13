49d27bd3d93ff4b31b9a456212b82ef7755623ec
allen kim <allenkim@allens-MacBook-Air.local>
2022-12-13 11:33:20 -0500

    preCondition

---
+++ src/custom-element.getting-started.stories.ts
@@ -0,0 +1,36 @@
+import { customElement  } from "./custom-element";
+
+customElement({
+  debug: true,
+  tagName: 'hello-custom-element',
+  html: `
+    <h1>{{hello}} {{world}}</h1>
+    <br/> helloWorld: {{helloWorld}}
+    <br/> prop1 : {{prop1}}
+  `,
+  css: `hello-custom-element {
+    color: red; 
+    display: block;
+  }`,
+  attrs : {
+    hello: 'Hello',
+    world: 'Custom Element',
+    helloWorld: {type: 'boolean'}
+  },
+  props: {
+    prop1: 0,
+  },
+});
+
+export default {
+  title: 'customElement()/Getting Started'
+};
+
+export const Default = () => `
+  <hello-custom-element></hello-custom-element>
+`
+export const AttrsProps = () => `
+  <hello-custom-element id="test" hello="Hi," world="My Element" hello-world></hello-custom-element>
+  <button onclick="document.querySelector('#test').setAttribute('world', Math.random())">Change Attribute</button>
+  <button onclick="document.querySelector('#test').prop1 = Math.random()">Change prop1</button>
+`;
